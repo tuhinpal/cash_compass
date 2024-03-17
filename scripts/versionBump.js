a6143acc9d67235e67d11e8087ba760fcd31084b
tuhinpal <me@thetuhin.com>
2024-03-17 10:58:59 +0530

    Version bump with version code

--- scripts/versionBump.js
+++ scripts/versionBump.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const { execSync } = require("child_process");
 
 const getCurrentVersion = () => {
   const pubspec = fs.readFileSync("pubspec.yaml", "utf8");
@@ -7,7 +8,9 @@ const getCurrentVersion = () => {
 };
 
 const getNewVersion = (currentVersion) => {
-  let [major, minor, patch] = currentVersion.split(".").map((v) => parseInt(v));
+  let [major, minor, patch, versionCode = 0] = currentVersion
+    .split(/\.|\+/)
+    .map((v) => parseInt(v));
 
   if (patch >= 9) {
     minor++;
@@ -16,24 +19,32 @@ const getNewVersion = (currentVersion) => {
     patch++;
   }
 
-  return `${major}.${minor}.${patch}`;
+  return {
+    version: `${major}.${minor}.${patch}`,
+    versionCode: versionCode + 1,
+  };
 };
 
 const updatePubSpec = (newVersion) => {
   const pubspec = fs.readFileSync("pubspec.yaml", "utf8");
-  const newPubspec = pubspec.replace(/version: (.*)/, `version: ${newVersion}`);
+  const newPubspec = pubspec.replace(
+    /version: (.*)/,
+    `version: ${newVersion.version}+${newVersion.versionCode}`
+  );
   fs.writeFileSync("pubspec.yaml", newPubspec);
 };
 
 const updateReadme = (newVersion) => {
   const readme = fs.readFileSync("README.md", "utf8");
-  const newReadme = readme.replace(/v\d+\.\d+\.\d+/g, `v${newVersion}`);
+  const newReadme = readme.replace(/v\d+\.\d+\.\d+/g, `v${newVersion.version}`);
   fs.writeFileSync("README.md", newReadme);
 };
 
 const currentVersion = getCurrentVersion();
 const newVersion = getNewVersion(currentVersion);
-console.log(`Bumping version from ${currentVersion} to ${newVersion}`);
+console.log(`Bumping version from ${currentVersion} to ${newVersion.version}`);
 
 updateReadme(newVersion);
 updatePubSpec(newVersion);
+
+execSync("flutter pub get");
